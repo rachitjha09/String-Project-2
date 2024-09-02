ee9f401775c52bdfa121120b67e12d25d5d851d9
rachitjha09 <rachitjha09@gmail.com>
2024-09-02 19:39:25 -0400

    Add files via upload

---
+++ CountHi.java
@@ -0,0 +1,22 @@
+
+/**
+ * String 2 Problem
+ * Return the number of times that the string "hi" appears anywhere in the given string.
+ *
+ * @author (Rachit Jha)
+ * @version (11/14/23)
+ */
+public class CountHi
+{
+    public static int countHi(String str){
+        int answer;
+        for(int i=0; i<str.length(); i++){
+            if(
+        }
+    }
+    public static void main(String[]args){
+        String s="xxhixxx";
+        int count=countHi(s);
+        System.out.println("count is " + count);
+    }
+}
